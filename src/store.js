600f7ae8c38d2923e64b0ea943a68026bc0c7e89
KHD <nlom0218@naver.com>
2021-03-17 21:56:05 +0900

    finished

--- src/store.js
+++ src/store.js
@@ -1,32 +1,19 @@
 import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const ADD = "add";
-const DELETE = "delete";
+const toDos = createSlice({
+  name: "toDoReducer",
+  initialState: [],
+  reducers: {
+    add(state, action) {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove(state, action) {
+      return state.filter((toDo) => toDo.id !== action.payload);
+    },
+  },
+});
 
-const addTodo = (text) => {
-  return { type: ADD, text };
-};
+export const { add, remove } = toDos.actions;
 
-const deleteToDo = (id) => {
-  return { type: DELETE, id: parseInt(id) };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-export const actionCreators = {
-  addTodo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
